6b9e29541f88401245ada75afaf1a98b594f5742
olgasvitlychna <olga.svitlychna9@gmail.com>
2021-10-26 14:07:18 +0200

    add @media

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
 let containerWithItems = document.getElementById('containerWithItems');
 
-let num = 1;
-for (let i = 0; i < 10; i++){
+let evenNumbersContainer = document.createElement('div');
+evenNumbersContainer.setAttribute('class', 'evenNum');
+containerWithItems.appendChild(evenNumbersContainer);
 
-    // create div element
 
-    let divElement = document.createElement('div');
-    divElement.setAttribute('class', 'post');
+let oddNumbersContainer = document.createElement('div');
+oddNumbersContainer.setAttribute('class', 'oddNum');
+containerWithItems.appendChild(oddNumbersContainer);
 
-    containerWithItems.appendChild(divElement);
 
-    divElement.addEventListener('click', () => {
-        alert(`Position number is: ${positionNumber}`)
-    })
-    // create item inside div
+for (let i = 0; i < 10; i++) {
+    let positionNumber = i + 1;
 
-    let postTitle = document.createElement('p');
-    postTitle.setAttribute('class', 'postTitle');
-    postTitle.innerText = 'item';
+    if (i % 2 == 0) {
+        // create div element with even numbers
 
-    divElement.appendChild(postTitle);
+        let evenNumbers = document.createElement('div');
+        evenNumbers.setAttribute('class', 'post');
 
-    // create paragrap with counter inside div
-    let positionNumber = num++
-    let positionCounter = document.createElement('p');
-    positionCounter.setAttribute('class', 'postCounter');
-    positionCounter.innerText = `Pos:${positionNumber}`
-    divElement.appendChild(positionCounter)
+        evenNumbersContainer.appendChild(evenNumbers);
+        evenNumbers.addEventListener('click', () => {
+            alert(`Position number is: ${positionNumber}`)
+        })
 
-  
+        let postTitle = document.createElement('h4');
+        postTitle.setAttribute('class', 'postTitle');
+        postTitle.innerText = 'item';
+
+        evenNumbers.appendChild(postTitle);
+
+        let positionCounter = document.createElement('p');
+        positionCounter.setAttribute('class', 'postCounter');
+        positionCounter.innerText = `Pos:${positionNumber}`
+        evenNumbers.appendChild(positionCounter)
+    } else {
+        // create div element with odd numbers
+        let oddNumbers = document.createElement('div');
+        oddNumbers.setAttribute('class', 'post');
+
+        oddNumbersContainer.appendChild(oddNumbers);
+        oddNumbers.addEventListener('click', () => {
+            alert(`Position number is: ${positionNumber}`)
+        })
+
+        let postTitle = document.createElement('h4');
+        postTitle.setAttribute('class', 'postTitle');
+        postTitle.innerText = 'item';
+
+        oddNumbers.appendChild(postTitle);
+
+        let positionCounter = document.createElement('p');
+        positionCounter.setAttribute('class', 'postCounter');
+        positionCounter.innerText = `Pos:${positionNumber}`
+        oddNumbers.appendChild(positionCounter)
+    }
 }
 
 
